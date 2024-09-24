5c75e8cb78fe0e415779e63ff6d6467cfcead33d
Benjamin Saks <saksben2@gmail.com>
2024-09-23 19:20:17 -0700

    add icons

---
+++ src/assets/index.ts
@@ -0,0 +1,46 @@
+import Facebook from "./svg/facebook.svg";
+import Twitter from "./svg/twitter.svg";
+import Pinterest from "./svg/pinterest.svg";
+import Instagram from "./svg/instagram.svg";
+import Youtube from "./svg/youtube.svg";
+import AmericanExpress from "./svg/american-express.svg";
+import ApplePay from "./svg/apple-pay.svg";
+import CartPay from "./svg/cart.svg";
+import DinersClub from "./svg/diners-club.svg";
+import Discover from "./svg/discover.svg";
+import GooglePay from "./svg/google-pay.svg";
+import Mastercard from "./svg/mastercard.svg";
+import MetaPay from "./svg/meta-pay.svg";
+import Paypal from "./svg/paypal.svg";
+import ShopPay from "./svg/shop-pay.svg";
+import Venmo from "./svg/venmo.svg";
+import SearchIcon from "./svg/search.svg";
+import LoginIcon from "./svg/login.svg";
+import CartIcon from "./svg/cart.svg";
+import Visa from './svg/visa.svg'
+
+export {
+  // Social Media icons
+  Facebook,
+  Twitter,
+  Pinterest,
+  Instagram,
+  Youtube,
+  // Payment Method Icons
+  AmericanExpress,
+  ApplePay,
+  CartPay,
+  DinersClub,
+  Discover,
+  GooglePay,
+  Mastercard,
+  MetaPay,
+  Paypal,
+  ShopPay,
+  Venmo,
+  Visa,
+  // Nav icons
+  SearchIcon,
+  LoginIcon,
+  CartIcon,
+};
